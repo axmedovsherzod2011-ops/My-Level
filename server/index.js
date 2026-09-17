0d22b5a70bd75c113372f23c37a2d55e72924baa
axmedovsherzod2011-ops <axmedovsherzod2011@gmail.com>
2026-09-17 14:45:20 +0500

    Fix device dashboard CORS for owner token

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin || "*");
     res.setHeader("Vary", "Origin");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Owner-Token");
   }
   if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
